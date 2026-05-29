335d6ba7eba51812125291cd298194509c34b592
Nikitiyy <nikita184615@gmail.com>
2026-05-29 17:12:56 +0000

    turn on website

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
-const API_URL = "https://abcd-12-34-56-78.ngrok-free.app";
+const API_URL = "https://quarterly-scotland-off-demonstrates.trycloudflare.com://abcd-12-34-56-78.ngrok-free.app";
 
 export default API_URL;
